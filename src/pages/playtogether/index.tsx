11372dde4bd2e65a96aae910340d5e75c9ebabef
Mateus Franco <mateusfranc2@gmail.com>
2024-04-12 20:16:15 -0300

    Playtogether

---
+++ src/pages/playtogether/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container, Title } from "./styled";
+
+export default function PlayTogether() {
+return <Container>
+  <Title>Bom Game Dupla</Title>
+</Container>;
+}
